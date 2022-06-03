31e1b57436c78a06aa3023db8b2b5422b741a250
devteam <devteam@cynosurenetworx.com>
2022-06-03 09:17:52 -0400

    First commit

---
+++ wp-content/cache/acorn/framework/views/a9f3c56e647bf78d2028a6ca9c690026dfe8ca65.php
@@ -0,0 +1,2 @@
+<?php (dynamic_sidebar('sidebar-primary')); ?>
+<?php /**PATH /home/cabox/workspace/wp-content/themes/CynosurePress/resources/views/sections/sidebar.blade.php ENDPATH**/ ?>
---
+++ wp-content/cache/acorn/framework/views/d33b2916736564db9e65e9df3a3acf3a44034016.php
@@ -0,0 +1,4 @@
+<div class="page-header">
+  <h1><?php echo $title; ?></h1>
+</div>
+<?php /**PATH /home/cabox/workspace/wp-content/themes/CynosurePress/resources/views/partials/page-header.blade.php ENDPATH**/ ?>
